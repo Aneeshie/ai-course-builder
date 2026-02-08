ab86384042dd0a2755822ac3cf3aadd1c8b846f1
Aneeshie <aneeshdas556@gmail.com>
2026-02-08 23:43:09 +0530

    fix: drizzle client format

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -5,6 +5,10 @@ import { config } from "dotenv";
 
 config({ path: ".env.local" });
 
-const client = neon(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL environment variable is not set")
+}
 
-export const db = drizzle(client, { schema });
+const client = neon(process.env.DATABASE_URL);
+
+export const db = drizzle({ client, schema });
