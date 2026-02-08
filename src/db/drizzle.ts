8b21c10700727078de3072cd6fbe9221989241d9
Aneeshie <aneeshdas556@gmail.com>
2026-02-08 23:33:19 +0530

    fix: initialize drizzle client with neon adapter

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,9 +1,10 @@
-import {config} from "dotenv"
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema";
+import { config } from "dotenv";
 
-import {drizzle} from 'drizzle-orm/neon-http'
-import * as schema from "./schema"
+config({ path: ".env.local" });
 
+const client = neon(process.env.DATABASE_URL!);
 
-config({path: ".env.local"});
-
-export const db = drizzle(process.env.DATABASE_URL!, {schema});
+export const db = drizzle(client, { schema });
